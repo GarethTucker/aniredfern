3c4252944877844024d3a927d691efd00a9e4f44
unknown <garethtucker2012@gmail.com>
2018-06-10 19:38:24 +1200

    Fixed nav bar and parallax

--- js/test.js
+++ js/test.js
@@ -16,24 +16,24 @@ $(document).ready(function(){
     });    
 });
 
-//menu transition js
-$(document).ready(function(){
-  $(window).scroll(function(){
-  	var scroll = $(window).scrollTop();
-    	  if (scroll > 0) {
-	    $(".navbar").addClass("navbar-scroll");
-        }
-    	  else{
-		  $(".navbar").removeClass("navbar-scroll");  	
-	  }
-	  if (scroll > 200) {
-	    $(".navbar").addClass("bg-primary");
-	  }
+// //menu transition js
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//   	var scroll = $(window).scrollTop();
+//     	  if (scroll > 0) {
+// 	    $(".navbar").addClass("navbar-scroll");
+//         }
+//     	  else{
+// 		  $(".navbar").removeClass("navbar-scroll");  	
+// 	  }
+// 	  if (scroll > 200) {
+// 	    $(".navbar").addClass("bg-primary");
+// 	  }
 
-	  else{
-		  $(".navbar").removeClass("bg-primary");  	
-	  }
-  })
-})
+// 	  else{
+// 		  $(".navbar").removeClass("bg-primary");  	
+// 	  }
+//   })
+// })
 
 
